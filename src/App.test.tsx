5b450da79b97904711c6c608979cdec62632d176
amine <amine.ameziane@mosaiclab.io>
2020-10-21 19:45:56 +0100

    husky pre-commit hook

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+// test('renders react boilerplate title', () => {
+//   const { getByText } = render(<App />);
+//   const title = getByText(/React Boilerplate/i);
+//   expect(title).toBeInTheDocument();
+// });
+
+it('renders react boilerplate title', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(getByText('React Boilerplate')).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h3>Dashboard</h3>
+      <h1>React Boilerplate</h1>
     </div>
   );
 }
